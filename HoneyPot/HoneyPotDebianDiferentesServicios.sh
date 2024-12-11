344f18eea7a0c43abefca310811e35626f57020e
LilBro <alejandroalo33@proton.me>
2024-12-11 21:51:52 +0100

    HoneyPotDebianDiferentesServicios.sh

--- HoneyPot/HoneyPotDebianDiferentesServicios.sh
+++ HoneyPot/HoneyPotDebianDiferentesServicios.sh
@@ -42,20 +42,25 @@ function print_message() {
 # ACTUALIZAR EL SISTEMA
     print_message "Actualizando el sistema dentro del contenedor..."
     apt update && apt upgrade -y
+    print_message "Sistema actualizado..."
 
 # INSTALAR LOS PROGRAMAS NECESARIOS
-    print_message "Instalando programas necesarios para el correcto funcionamiento del honeypot..."
-    apt install openssh-server -y && apt install nginx -y && apt install curl -y && apt install tcpdump -y && apt install rsyslog -y && apt install ufw -y && apt install vsftpd -y && apt install mariadb-server -y
+    print_message "Descargando servicios necesarios para el correcto funcionamiento del honeypot..."
+    apt install openssh-server -y && apt install nginx -y && apt install curl -y && apt install tcpdump -y && apt install rsyslog -y && apt install ufw -y && apt install vsftpd -y && apt install mariadb-server -y \
+    apt install git -y
+    print_message "Servicios necesarios descargados..."
 
 # MODIFICACIÓN DEL FICHERO SSH PARA HACERLO PARECER VULNERABLE
     print_message "Modificando configuraciones de SSH para simular vulnerabilidades..."
     sed -i -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
     sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
     sed -i -e 's/#LogLevel INFO/LogLevel VERBOSE/g' /etc/ssh/sshd_config
+    print_message "SSH vulnerable creado..."
 
 # REINICIO DEL SERVICIO SSH
     print_message "Reiniciando el servicio SSH..."
     /etc/init.d/ssh restart
+    print_message "Servicio Reiniciado..."
 
 # CREACIÓN DE USUARIOS Y CONTRASEÑAS INSEGURAS
     print_message "Creando usuarios con contraseñas débiles..."
@@ -63,25 +68,32 @@ function print_message() {
     useradd -m user1
     echo "user:P@ssw0rd!" | chpasswd
     echo "user:P@ssw0rd!" >> /etc/passwords.txt
+    print_message "Usuarios y contraseñas creadas correctamente..."
 
 # CONFIGURACIÓN DEL SERVICIO WEB
-print_message "Configurando el servidor web..."
-curl 
-/etc/init.d/nginx restart
+    print_message "Configurando el servidor web..."
+    print_message "Clonando el repositorio de la página web..."
+    git clone https://github.com/L1LBRO/CTF.git
+    mv CTF/HoneyPot/HoneyPotPage /var/www/html/
+    rm -r CTF
+    
+# REINICIO DEL SERVICIO WEB
+    print_message "Reinicio de NGINX..."
+    /etc/init.d/nginx restart
 
-# Configurar FTP
-print_message "Configurando el servidor FTP..."
-sed -i -e 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd.conf
-sed -i -e 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
+# CONFIGURAR SERVIDOR FTP
+    print_message "Configurando el servidor FTP..."
+    sed -i -e 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd.conf
+    sed -i -e 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
 
-# Crear carpetas FTP
+# CREAR CARPETAS FTP
 print_message "Creando carpetas públicas en el servidor FTP..."
 mkdir -p /var/ftp/pub
 touch /var/ftp/pub/datos_empresita.txt
 touch /var/ftp/pub/empleados_empresita.txt
 systemctl restart vsftpd
 
-# Crear servicio de monitorización
+# CREAR SERVICIO EN SYSTEMD PARA MONITORIZAR
 print_message "Creando servicio de monitorización de tráfico SSH y HTTP..."
 cat << EOT > /etc/systemd/system/tcpdump-ssh-http.service
 [Unit]
@@ -102,13 +114,13 @@ SyslogIdentifier=tcpdump-ssh-http
 WantedBy=multi-user.target
 EOT
 
-# Reiniciar systemd y activar el servicio
+# REINICIAR SYSTEMD Y ACTIVANDO EL SERVICIO RECIEN CREADO
 print_message "Activando el servicio de monitorización..."
 systemctl daemon-reload
 systemctl enable tcpdump-ssh-http.service
 systemctl start tcpdump-ssh-http.service
 
-# Configuración de firewall
+# CONFIGURACIÓN BÁSICA DEL FW
 print_message "Configurando el firewall..."
 ufw allow 22
 ufw allow 80
